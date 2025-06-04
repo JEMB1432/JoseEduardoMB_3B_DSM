67c8950b066b8c21636c344199ff67a60457ba77
JEMB1432 <20243ds041@utez.edu.mx>
2025-06-04 08:48:33 -0600

    Programa 6

---
+++ U1_TI_06_ContrasenaSegura/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        UsuarioSeguro usuarioSeguro = new UsuarioSeguro();
+
+        System.out.println("Introduzca el usuario: ");
+        String usuario = sc.nextLine();
+        usuarioSeguro.setNombreUsuario(usuario);
+
+        System.out.println("Introduzca el password: ");
+        String password = sc.nextLine();
+        usuarioSeguro.setContrasena(password);
+
+        if(!usuarioSeguro.estaInicializado()){
+            System.out.println("Ingrese correctamente los datos");
+        }else {
+            System.out.println("Ingrese su contraseña");
+            String contrasena = sc.nextLine();
+
+            if(usuarioSeguro.autenticar(contrasena)) {
+                System.out.println("Acceso concedido \n");
+            } else {
+                System.out.println("Acceso denegado \n");
+            }
+        }
+    }
+}
