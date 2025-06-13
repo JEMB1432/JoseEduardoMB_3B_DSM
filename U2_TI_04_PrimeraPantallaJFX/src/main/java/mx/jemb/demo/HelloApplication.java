3d0ae2a5837b990e001ab4bc3ae5c30661f3adc3
JEMB1432 <20243ds041@utez.edu.mx>
2025-06-13 08:25:23 -0600

    Form

--- U2_TI_04_PrimeraPantallaJFX/src/main/java/mx/jemb/demo/HelloApplication.java
+++ U2_TI_04_PrimeraPantallaJFX/src/main/java/mx/jemb/demo/HelloApplication.java
@@ -1,12 +1,15 @@
 package mx.jemb.demo;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +18,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        /*
         Label label = new Label("Label 1");
         Label label2 = new Label("Labe2");
         Button button = new Button("Button Aceptar");
@@ -27,15 +31,58 @@ public class HelloApplication extends Application {
         button2.setOnAction(e -> {
 
             label.setText("Cancelado!");
+        });*/
+
+        Label lblNombre = new Label("Nombre: ");
+        TextField tfNombre = new TextField();
+        Label lblApellido = new Label("Apellido: ");
+        TextField tfApellido = new TextField();
+        Label lblComentario = new Label("Comentario: ");
+        TextArea TAComentario = new TextArea();
+        Label lblGrupo = new Label("Grupo: ");
+        ObservableList<String> grupos = FXCollections.observableArrayList("3A", "3B", "3C");
+        ComboBox<String> cbGrupo = new ComboBox<>(grupos);
+        Label lblResultado = new Label();
+
+        Button btnEnviar = new Button("Enviar");
+        btnEnviar.setOnAction(e -> {
+            String nombre = tfNombre.getText();
+            String apellido = tfApellido.getText();
+            String comentario = TAComentario.getText();
+            String grupo = cbGrupo.getSelectionModel().getSelectedItem();
+            if(nombre.isEmpty() || apellido.isEmpty() || comentario.isEmpty()) {
+                lblResultado.setText("Llene todos los campos");
+            }else {
+                lblResultado.setText("Nombre: " + nombre + "\nApellido: " + apellido + "\nComentario: " + comentario + "\nGrupo: " + grupo );
+            }
         });
 
-        VBox vbox = new VBox(20,label, label2, button, button2);
-        vbox.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(vbox, 500, 240);
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+
+        root.add(lblNombre, 0, 0);
+        root.add(tfNombre, 1, 0);
+        root.add(lblApellido, 0, 1);
+        root.add(tfApellido, 1, 1);
+        root.add(lblComentario, 0, 2);
+        root.add(TAComentario, 1, 2);
+        root.add(lblGrupo, 0, 3);
+        root.add(cbGrupo, 1, 3);
+        root.add(btnEnviar, 0, 4);
+        root.add(lblResultado, 1, 5);
+
+        //VBox vbox = new VBox(20);
+        //vbox.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(root, 800, 600);
+
+
 
         stage.setTitle("Hello!");
         Image image = new Image(getClass().getResourceAsStream("/images/conf.png"));
         stage.getIcons().add(image);
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
